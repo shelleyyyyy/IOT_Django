cadd72671dede94c3712f469ee72ce1dcc155bb3
shelleyyyyy <shelleywr23@mail.vmi.edu>
2021-12-18 23:55:25 -0500

    django setup

--- IOT_Django/asgi.py
+++ IOT_Django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for IOT project.
+ASGI config for IOT_Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IOT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IOT_Django.settings')
 
 application = get_asgi_application()
